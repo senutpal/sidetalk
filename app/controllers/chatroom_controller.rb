888e96c1ac71caa825e7b0090fea2d6fde058159
senutpal <utpalsen902@gmail.com>
2026-02-12 15:21:19 +0000

    add user authentication and chat messaging

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,4 +1,7 @@
 class ChatroomController < ApplicationController
   def index
+    @messages = Message.includes(:user).order(created_at: :asc)
+    @users = User.all
   end
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+users = [
+  { username: 'admin', password: 'password123' },
+  { username: 'moderator', password: 'password123' },
+  { username: 'sarah_j', password: 'hello123' },
+  { username: 'mike_t', password: 'hello123' },
+  { username: 'alex_k', password: 'hello123' }
+]
+
+created_users = users.map do |u|
+  User.find_or_create_by!(username: u[:username]) do |user|
+    user.password = u[:password]
+  end
+end
+
+messages = [
+  { user: created_users[0], body: 'Welcome everyone to Sidetalk!' },
+  { user: created_users[1], body: 'Glad to have you all here' },
+  { user: created_users[2], body: 'This chat looks great!' },
+  { user: created_users[3], body: 'Hey @senutpal, thanks for setting this up' },
+  { user: created_users[2], body: 'No problem! Happy to chat' },
+  { user: created_users[4], body: 'Love the UI design' },
+  { user: created_users[0], body: 'We have more features coming soon' },
+  { user: created_users[1], body: 'Stay tuned for updates!' },
+  { user: created_users[3], body: "Can't wait to see what's next" },
+  { user: created_users[2], body: 'Same here!' }
+]
+
+messages.each do |m|
+  Message.find_or_create_by!(body: m[:body], user: m[:user])
+end
